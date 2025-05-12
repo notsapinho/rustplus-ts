e35f69e412da6054c22e36ba385ff7401a419f13
Alexander Emanuelsson <alexander.emanuelsson94@gmail.com>
2025-05-12 21:49:32 +0200

    Quickfix for corrupt responses

--- src/rustplus.ts
+++ src/rustplus.ts
@@ -424,8 +424,30 @@ export class RustPlus extends EventEmitter {
                     throw new Error('Received empty or invalid message data.');
                 }
 
+                let messageData: Uint8Array;
+                if (Buffer.isBuffer(data)) {
+                    messageData = new Uint8Array(data);
+                    /* Ensures incoming WebSocket message data is at least 10 bytes long by copying it into a new
+                    zero-padded Uint8Array if it's shorter than expected. This avoids protobufjs RangeError crashes
+                    caused by malformed or truncated binary payloads. */
+                    if (messageData.length < 10) {
+                        const paddedData = new Uint8Array(10);
+                        paddedData.set(messageData)
+                        messageData = paddedData;
+                    }
+                }
+                else if (data instanceof ArrayBuffer) {
+                    messageData = new Uint8Array(data);
+                }
+                else if (typeof data === 'string') {
+                    messageData = new TextEncoder().encode(data);
+                }
+                else {
+                    throw new Error('Unsupported data format.');
+                }
+
                 let handled = false;
-                const appMessage: rpi.AppMessage = rpi.AppMessage.fromBinary(data as any);
+                const appMessage: rpi.AppMessage = rpi.AppMessage.fromBinary(messageData);
 
                 if (appMessage.response && this.seqCallbacks[appMessage.response.seq]) {
                     const callback: CallbackFunction = this.seqCallbacks[appMessage.response.seq];
