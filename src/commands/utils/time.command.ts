6fe4a002b9f780dba2eb70c08869fa71891bc64e
notsapinho <52896767+notsapinho@users.noreply.github.com>
2025-09-06 11:22:24 -0300

    fix: wrong time converter

--- src/commands/utils/time.command.ts
+++ src/commands/utils/time.command.ts
@@ -1,7 +1,7 @@
 import type { CommandContext } from "@/lib/structures/command";
 
 import { Command } from "@/lib/structures/command";
-import { decimalToHHMM, decimalToHM, minutesToMMSS } from "@/lib/utils";
+import { decimalToHHMM, minutesToMMSS } from "@/lib/utils";
 
 export class UtilCommand extends Command {
     public constructor(context: CommandContext) {
@@ -15,7 +15,7 @@ export class UtilCommand extends Command {
             `Agora são ${decimalToHHMM(this.container.client.time.time)} | ${
                 this.container.client.time.isDaytime
                     ? `${minutesToMMSS(this.container.client.time.realPerHour * this.container.client.time.inGameTimeUntilNighttime)} até o anoitecer.`
-                    : `${decimalToHM(
+                    : `${minutesToMMSS(
                           this.container.client.time.realPerHour *
                               this.container.client.time.inGameTimeUntilDaytime
                       )} até o amanhecer.`
